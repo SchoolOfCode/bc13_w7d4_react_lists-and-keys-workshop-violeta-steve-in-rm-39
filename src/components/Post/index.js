cd969a5c26bcda74da582feba3ac9549f35fd5fb
Sunbearian <stigcollins@icloud.com>
2022-11-10 17:11:08 +0000

    Task 6 complete

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,17 +2,19 @@ import React from "react";
 
 import "./index.css";
 
-function Post({title, author, date, image, text, highlights}) {
-	return <article>
-		<h1>{title}</h1>
-		<h2>{author}</h2>
-		<p>{date}</p>
-		<img src={image.link} alt={image.alt}></img>
-		<p>{text}</p>
-		{highlights.map((highlight) => {
-			return <li>{highlight}</li> 
-		})}
-	</article>;
+function Post({ title, author, date, image, text, highlights }) {
+	return (
+		<article>
+			<h1>{title}</h1>
+			<h2>{author}</h2>
+			<p>{date}</p>
+			<img src={image.link} alt={image.alt} height="350px" width="auto"></img>
+			<p>{text}</p>
+			{highlights.map((highlight) => {
+				return <li>{highlight}</li>;
+			})}
+		</article>
+	);
 }
 
 export default Post;
